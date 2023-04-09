692bfc780169908f4abf3e7273f9f99859a7274f
Stan <stephenppercival@gmail.com>
2023-04-09 18:37:09 +0100

    vMedia item detail

---
+++ src/pages/vMedia/[id].tsx
@@ -0,0 +1,32 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+import { trpc } from "@utils/trpc";
+import { useDefinedIdRoute } from "@utils/hooks/useDefinedIdRoute";
+
+const Detail: NextPage = () => {
+  const { id, loading } = useDefinedIdRoute();
+
+  if (loading) {
+    return <></>;
+  }
+
+  const vMediaDetail = trpc.vMedia.getVMediaById.useQuery(id);
+  return (
+    <>
+      <Head>
+        <title>Books</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        {vMediaDetail.isSuccess && (
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            {vMediaDetail.data?.title}
+          </h1>
+        )}
+      </main>
+    </>
+  );
+};
+
+export default Detail;
